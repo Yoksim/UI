4ac85c008dd80dbe090986d062177618656bc003
Glen Low <glen.low.2021@scis.smu.edu.sg>
2023-06-21 16:44:51 +0800

    Update URL

--- mimusa/main_ui.js
+++ mimusa/main_ui.js
@@ -1,4 +1,4 @@
-const mimusa_python_code_URL = "http://127.0.0.1:8000/generate"
+const mimusa_python_code_URL = "https://mimusa-test-model.onrender.com:8000/generate"
 
 const app = Vue.createApp({
 
